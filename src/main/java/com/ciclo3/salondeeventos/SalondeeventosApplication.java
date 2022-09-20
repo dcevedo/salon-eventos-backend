a89bdb6b1f90ec0cb260f95dc47f771551f5c9cb
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-09-20 12:18:59 -0500

    First commit

---
+++ src/main/java/com/ciclo3/salondeeventos/SalondeeventosApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3.salondeeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalondeeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalondeeventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciclo3/salondeeventos/SalondeeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.salondeeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalondeeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
